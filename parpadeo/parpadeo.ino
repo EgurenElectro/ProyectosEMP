9e8c1416a3c1734f83f5b5340bb64c50fa619951
gabrielbeguren <gabrielbeguren@outlook.com>
2025-01-13 12:48:12 +0100

    first

--- parpadeo/parpadeo.ino
+++ parpadeo/parpadeo.ino
@@ -1,19 +1,45 @@
-//Programa: parpadeo.ino (placa esp32 + shield)
+//Programa: intermitente.ino (placa esp32 + shield)
 //Autor: Gabriel Bravo Eguren - Enero 2025
 
-#define PLED 12
+#define D1 12
+#define D2 13
+#define D3 5
+#define D4 23
+
+#define DELAY 100
+
 
 void setup() 
 {
-  pinMode(PLED, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
 
 }
 
 void loop() 
 {
-  digitalWrite(PLED, HIGH);
-  delay(1000);
-  digitalWrite(PLED, LOW);
-  delay(1000);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);
+
+  delay(DELAY);
+
+  digitalWrite(D1, HIGH);
+
+  delay(DELAY);
+
+  digitalWrite(D2, HIGH);
+
+
+  delay(DELAY);
+
+  digitalWrite(D3, HIGH);
+
+  delay(DELAY);
+
+  digitalWrite(D4, HIGH);
 
 }
